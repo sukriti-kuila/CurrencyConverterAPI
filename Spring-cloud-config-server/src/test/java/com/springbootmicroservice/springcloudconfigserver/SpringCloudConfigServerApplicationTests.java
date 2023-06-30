1ec3f3ef06349daad32f0c2923810915468eb14d
Sukriti Kuila <87015685+sukriti-kuila@users.noreply.github.com>
2023-06-30 15:10:07 +0530

    Add files via upload

---
+++ Spring-cloud-config-server/src/test/java/com/springbootmicroservice/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootmicroservice.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
